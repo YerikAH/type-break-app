cc657ed3929b73a19f2afc17bf75922b257cb468
Harvey Yerik <yerik05vh@gmail.com>
2022-12-16 17:14:26 -0500

    add logic

--- version/v0.2/src/components/LogicApp.jsx
+++ version/v0.2/src/components/LogicApp.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import "./logicStyle.css";
 
 export default function LogicApp() {
+  const [word, setWord] = useState([]);
+  const [wordWrite, setWordWrite] = useState("");
+  const [followIndex, setFollowIndex] = useState(0);
+  const [lesscount, setLesscount] = useState(0)
+  const [countSpace, setCountSpace] = useState(0)
+
   async function getDataWord() {
     let url = "./src/data/easyEsp.json";
     try {
@@ -14,27 +21,76 @@ export default function LogicApp() {
           resStatusText: res.statusText,
         };
       }
-      console.log(json);
+      let fullLetter = [];
+      json.forEach((element) => {
+        const wordSeparate = element.word;
+        for (let i = 0; i < wordSeparate.length; i++) {
+          const item = wordSeparate[i];
+          const createLetter = {
+            letterElement: item,
+            id: crypto.randomUUID(),
+          };
+          fullLetter.push(createLetter);
+        }
+      });
+      setWord(fullLetter);
     } catch (err) {}
   }
-  const [word, setWord] = useState([]);
-  const [wordWrite, setWordWrite] = useState("");
 
   const handleChange = (e) => {
-    setWordWrite(e.target.value);
+    const valueCapture = e.target.value;
+    let valueCaptureLength = (valueCapture.length - 1) - lesscount
+    setWordWrite(valueCapture);
+
+    if (
+      valueCapture.slice(-1) === word[followIndex].letterElement &&
+      valueCaptureLength === followIndex && lesscount === countSpace
+    ) {
+      if(followIndex%4===3){
+        setCountSpace(countSpace + 1 )
+      }
+
+
+      setFollowIndex(followIndex + 1);
+      console.log("Check");
+    } else if (valueCapture.slice(-1) === " ") {
+      if(followIndex%4===3){
+        setLesscount(lesscount + 1)
+      }
+      console.log("espacio");
+    } else {
+
+      if (valueCaptureLength < followIndex) {
+        setFollowIndex(followIndex - 1);
+        console.log("Regresando");
+      }else{
+        console.log("Error");
+      }
+    }
+    console.log(lesscount)
+    console.log(countSpace)
+
+
+
+    // console.log("our write " + `${e.target.value[valueCaptureLength]}`);
+    // console.log("machine write " + `${word[followIndex].letterElement}`);
   };
 
   useEffect(() => {
-    const dataWord = getDataWord();
-    setWord(dataWord);
+    getDataWord();
   }, []);
 
   return (
     <div className="content-center">
       <p>
-        {word.map((item) => (
-          <i key={item.word}>{item.word}</i>
-        ))}
+        {word.map((item, k) => {
+          return (
+            <>
+              <i key={item.id}>{item.letterElement}</i>
+              {(k + 1) % 4 == 0 && " "}
+            </>
+          );
+        })}
       </p>
       <input type="text" value={wordWrite} onChange={handleChange} />
     </div>
